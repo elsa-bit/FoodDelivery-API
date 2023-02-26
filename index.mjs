977f18508d4caf130ee075a3e4cab5b1e87e891b
Jouskaio <jouskaio.me@gmail.com>
2023-02-26 22:05:24 +0100

    Create new delieverer

--- index.mjs
+++ index.mjs
@@ -4,6 +4,8 @@ import routerLogin from "./server/bossLogin.mjs";
 import routerDeliverers from "./server/deliverers.mjs";
 import routerDELeliver from "./server/delete/deliverer.mjs";
 import routerUPDDeliverer from "./server/update/deliverer.mjs";
+import routerADDDeliverer from "./server/add/deliverer.mjs";
+import routerLastLocation from "./server/location.mjs";
 
 const app = express()
 
@@ -12,6 +14,8 @@ app.use('/api', routerLogin);
 app.use('/api', routerDeliverers);
 app.use('/api', routerDELeliver);
 app.use('/api', routerUPDDeliverer);
+app.use('/api', routerADDDeliverer);
+app.use('/api', routerLastLocation);
 app.listen(2000, () => { 
     console.log("Serveur à l'écoute sur http://localhost:2000")       
 })
