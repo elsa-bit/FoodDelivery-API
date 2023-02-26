4eeaba784a43cebacddd5786d5da6ec47acb363d
Jouskaio <jouskaio.me@gmail.com>
2023-02-26 11:11:07 +0100

    Add delete function

--- index.mjs
+++ index.mjs
@@ -1,12 +1,15 @@
 import express from 'express'
 import routerPackage from './server/package.mjs';
 import routerLogin from "./server/bossLogin.mjs";
+import routerDeliverers from "./server/deliverers.mjs";
+import routerDELeliver from "./server/delDeliverer.mjs";
 
 const app = express()
 
 app.use('/api', routerPackage);
 app.use('/api', routerLogin);
-
+app.use('/api', routerDeliverers);
+app.use('/api', routerDELeliver);
 app.listen(2000, () => { 
     console.log("Serveur à l'écoute sur http://localhost:2000")       
 })
