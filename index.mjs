c9606433f8fdf701e4cffe5b4e2396153847adc6
tageldin <maaztageldin030@hotmail.com>
2023-02-26 01:14:39 +0100

    routerEmployer

--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
 import express from 'express'
+import routerEmployer from './server/employer.mjs'
+
 
 const app = express()
+app.use('/api', routerEmployer)
 
 app.listen(2000, () => { 
     console.log("Serveur à l'écoute sur http://localhost:2000")       
