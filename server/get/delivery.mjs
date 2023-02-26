bb872a2cc43a396afd97f834f557c674a47dd036
Elsa Firmin <elsafirmin@hotmail.fr>
2023-02-26 23:24:29 +0100

    add route for edit delivery

--- server/get/delivery.mjs
+++ server/get/delivery.mjs
@@ -29,4 +29,28 @@ routerDelivery.get('/employerDelivery', async (req, res) => {
     res.end
 });
 
+routerDelivery.put('/editDelivery', async (req, res) => {
+    try {
+        const id = req.query.idDelivery;
+        const city = req.query.city;
+        const date = req.query.date
+
+        await pools.query('UPDATE delivery SET delivery_date=$1, delivery_location=$2 WHERE id=$3', [date, city,
+            id]);
+            const verifyModificated = await pools.query('SELECT delivery_date, delivery_location, id_employer FROM delivery WHERE id=$1', [id]);
+            
+        if (city == verifyModificated.rows[0].delivery_location && date == verifyModificated.rows[0].delivery_date) {
+            res.json({ status: 200, delivery: "Success", error: null });
+        } else {
+            res.status(401).send("The modification could not be performed");
+        }
+        
+        pools.end;
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error server");
+    }
+    res.end
+});
+
 export default routerDelivery
